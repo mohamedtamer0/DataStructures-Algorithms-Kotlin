a6b25f4622812e07227eac7b77b5ddf2a6dd7493
mohamedtamer0 <mt4452242@gmail.com>
2022-02-20 19:10:58 +0200

    update

---
+++ src/main/kotlin/02-complexity/LogarithmicTime.kt
@@ -0,0 +1,44 @@
+package `02-complexity`
+
+/*
+fun linearContains(value: Int, numbers: List<Int>): Boolean {
+    for (element in numbers) {
+        if (element == value) {
+            return true
+        }
+    }
+    return false
+}
+ */
+
+fun pseudoBinaryContains(value: Int, numbers: List<Int>): Boolean {
+    if (numbers.isEmpty()) return false
+    val midIndex = numbers.size / 2
+    if (value <= numbers[midIndex]) {
+        for (index in 0..midIndex) {
+            if (numbers[index] == value) {
+                return true
+            }
+        }
+    } else {
+        for (index in midIndex until numbers.size) {
+            if (numbers[index] == value) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+
+fun main() {
+    /*
+    val numbers = listOf(1, 3, 56, 66, 68, 80, 99, 105, 450)
+    println(linearContains(3, numbers))
+     */
+
+    val numbers = listOf(1, 3, 56, 66, 68, 80, 99, 105, 450)
+    println(pseudoBinaryContains(22, numbers))
+
+
+}
