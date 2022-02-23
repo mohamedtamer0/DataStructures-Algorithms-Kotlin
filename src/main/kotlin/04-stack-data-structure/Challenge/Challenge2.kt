e38e8b98d9c890dbc82e596dd762461eb8874f73
mohamedtamer0 <mt4452242@gmail.com>
2022-02-23 22:07:31 +0200

    update

--- src/main/kotlin/04-stack-data-structure/Challenge/Challenge2.kt
+++ src/main/kotlin/04-stack-data-structure/Challenge/Challenge2.kt
@@ -1,2 +1,31 @@
 package `04-stack-data-structure`.Challenge
 
+import `04-stack-data-structure`.StackImpl
+
+/*:
+ # Stack Challenges
+ ## Challenge 2:
+    Check for balanced parentheses.
+    Given a string, check if there are `(` and `)` characters,
+    and return `true` if the parentheses in the string are balanced.
+    For example:
+    h((e))llo(world)() // balanced parentheses
+    (hello world // unbalanced parentheses
+ */
+
+fun String.checkParentheses(): Boolean {
+
+    val stack = StackImpl<Char>()
+    for (char in this) {
+        when (char) {
+            '(' -> stack.push(char)
+            ')' -> if (stack.isEmpty) {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+    }
+
+    return stack.isEmpty
+}
