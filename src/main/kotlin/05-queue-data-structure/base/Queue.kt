4381a888e0ea4ea8e6c62efe7b548094ef380024
mohamedtamer0 <mt4452242@gmail.com>
2022-02-23 22:22:14 +0200

    update

---
+++ src/main/kotlin/05-queue-data-structure/base/Queue.kt
@@ -0,0 +1,15 @@
+package `05-queue-data-structure`.base
+
+interface Queue<T : Any> {
+
+    fun enqueue(element: T): Boolean
+    fun dequeue(): T?
+
+    val count: Int
+        get
+
+    val isEmpty: Boolean
+        get() = count == 0
+
+    fun peek(): T?
+}
