ccabb1c0868f6a9b9314bc70b78d8cfb1ff93010
mohamedtamer0 <mt4452242@gmail.com>
2022-02-26 22:25:19 +0200

    update

---
+++ src/main/kotlin/06-trees/Main.kt
@@ -0,0 +1,39 @@
+package `06-trees`
+
+fun main() {
+
+    val tree = makeBeverageTree()
+    //tree.forEachDepthFirst { println(it.value) }
+    tree.forEachLevelOrder { println(it.value) }
+
+
+}
+
+fun makeBeverageTree(): TreeNode<String> {
+    val tree = TreeNode("Beverages")
+    val hot = TreeNode("hot")
+    val cold = TreeNode("cold")
+    val tea = TreeNode("tea")
+    val coffee = TreeNode("coffee")
+    val chocolate = TreeNode("cocoa")
+    val blackTea = TreeNode("black")
+    val greenTea = TreeNode("green")
+    val chaiTea = TreeNode("chai")
+    val soda = TreeNode("soda")
+    val milk = TreeNode("milk")
+    val gingerAle = TreeNode("ginger ale")
+    val bitterLemon = TreeNode("bitter lemon")
+    tree.add(hot)
+    tree.add(cold)
+    hot.add(tea)
+    hot.add(coffee)
+    hot.add(chocolate)
+    cold.add(soda)
+    cold.add(milk)
+    tea.add(blackTea)
+    tea.add(greenTea)
+    tea.add(chaiTea)
+    soda.add(gingerAle)
+    soda.add(bitterLemon)
+    return tree
+}
---
+++ src/main/kotlin/06-trees/TreeNode.kt
@@ -0,0 +1,34 @@
+package `06-trees`
+
+import `05-queue-data-structure`.base.list.ArrayListQueue
+
+
+typealias Visitor<T> = (TreeNode<T>) -> Unit
+
+class TreeNode<T : Any>(val value: T) {
+    private val children: MutableList<TreeNode<T>> = mutableListOf()
+
+    fun add(child: TreeNode<T>) = children.add(child)
+
+    fun forEachDepthFirst(visit: Visitor<T>) {
+        visit(this)
+        children.forEach {
+            it.forEachDepthFirst(visit)
+        }
+    }
+
+    fun forEachLevelOrder(visit: Visitor<T>) {
+        visit(this)
+        val queue = ArrayListQueue<TreeNode<T>>()
+        children.forEach { queue.enqueue(it) }
+
+        var node = queue.dequeue()
+        while (node != null) {
+            visit(node)
+            node.children.forEach { queue.enqueue(it) }
+            node = queue.dequeue()
+        }
+    }
+
+
+}
