29fe30b042127cf535ae84ccd1b2ba37de320aed
mohamedtamer0 <mt4452242@gmail.com>
2022-02-27 01:45:46 +0200

    update

---
+++ src/main/kotlin/07-binary-trees/Main.kt
@@ -0,0 +1,24 @@
+package `07-binary-trees`
+
+fun main() {
+    val zero = BinaryNode(0)
+    val one = BinaryNode(1)
+    val five = BinaryNode(5)
+    val seven = BinaryNode(7)
+    val eight = BinaryNode(8)
+    val nine = BinaryNode(9)
+
+    seven.leftChild = one
+    one.leftChild = zero
+    one.rightChild = five
+    seven.rightChild = nine
+    nine.leftChild = eight
+
+    val tree = seven
+
+    println(tree)
+
+    //tree.traverseInOrder { println(it) }
+    //tree.traversePreOrder { println(it) }
+    tree.traversePostOrder { println(it) }
+}
