f03e113e2331b25d0768d2c4e556a491f6401647
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2022-02-26 06:21:26 +0200

    refactor: use if as statement
    In Kotlin, `if` is an **expression**: it returns a value 
    
    for `message` part, i have added it, as we may want to do another handling for the result of when instead of print 
    so now we have full control and the code is scalable and maintainable.

--- src/main/kotlin/01-kotlin-and-kotlin-standard-library/Conditional.kt
+++ src/main/kotlin/01-kotlin-and-kotlin-standard-library/Conditional.kt
@@ -3,24 +3,21 @@ package `01-kotlin-and-kotlin-standard-library`
 fun main() {
     val a = 5
     val b = 12
-    var max = -1
-
-    if (a > b) {
-        max = a
+    
+    val maxNumber = if (a > b) {
+        a
     } else {
-        max = b
+        b
     }
 
-    println(max)
-
+    println(maxNumber)
 
     val groupSize = 3
-    when (groupSize) {
-        1 -> println("Single")
-        2 -> println("Pair")
-        3 -> {
-            println("Trio")
-        }
-        else -> println("This is either nobody or a big crowd")
+    val message = when (groupSize) {
+        1 -> "Single"
+        2 -> "Pair"
+        3 ->  "Trio"
+        else -> "This is either nobody or a big crowd"
     }
+    println(message)
 }
