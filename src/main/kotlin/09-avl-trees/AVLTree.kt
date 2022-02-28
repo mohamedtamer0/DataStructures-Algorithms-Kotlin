81bd9fd3961a95e643a3677009cbf66a136e7b2f
mohamedtamer0 <mt4452242@gmail.com>
2022-02-28 19:38:48 +0200

    update

--- src/main/kotlin/09-avl-trees/AVLTree.kt
+++ src/main/kotlin/09-avl-trees/AVLTree.kt
@@ -101,4 +101,48 @@ class AVLTree<T : Comparable<T>> {
         //5
         return pivot
     }
+
+
+    private fun rightRotate(node: AVLNode<T>): AVLNode<T> {
+        val pivot = node.leftChild!!
+        node.leftChild = pivot.rightChild
+        pivot.rightChild = node
+        node.height = max(node.leftHeight, node.rightHeight) + 1
+        pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
+        return pivot
+    }
+
+    private fun rightLeftRotate(node: AVLNode<T>): AVLNode<T> {
+        val rightChild = node.rightChild ?: return node
+        node.rightChild = rightRotate(rightChild)
+        return leftRotate(node)
+    }
+
+    private fun leftRightRotate(node: AVLNode<T>): AVLNode<T> {
+        val leftChild = node.leftChild ?: return node
+        node.leftChild = rightLeftRotate(leftChild)
+        return rightLeftRotate(node)
+    }
+
+    private fun balanced(node: AVLNode<T>): AVLNode<T> {
+        return when (node.balanceFactor) {
+            2 -> {
+                if (node.leftChild?.balanceFactor == -1) {
+                    leftRightRotate(node)
+                } else {
+                    rightRotate(node)
+                }
+            }
+            -2 -> {
+                if (node.rightChild?.balanceFactor == 1) {
+                    leftRightRotate(node)
+                }else {
+                    leftRotate(node)
+                }
+            }
+
+            else -> node
+        }
+    }
+
 }
